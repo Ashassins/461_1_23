769dc515df3f366369b89fe2d4f241ea10b24b12
Emile Baez <ebaezmunne@gmail.com>
2023-02-11 13:59:06 -0500

    addition of tests to manager and worker

--- testfiles/manager_test.go
+++ testfiles/manager_test.go
@@ -10,14 +10,11 @@ import (
 var url_ch_size = 100
 var rating_ch_size = 5
 
-// will check for activity on rating channel being filled up
-// func TestManagerStartsWorkers(t *testing.T) {
-
-// 	url_ch := make(chan string, url_ch_size)
-
-// }
-
-// testing for rating channel and workers
+/*
+testing for rating channel and workers
+expected behavior for StartWorkers():
+rating channel should be closed, tasks should be run filling up rating channel
+*/
 func TestManagerGoodInput(t *testing.T) {
 	url_ch := make(chan string, url_ch_size)
 	rating_ch := make(chan fileio.Rating, rating_ch_size)
@@ -27,17 +24,60 @@ func TestManagerGoodInput(t *testing.T) {
 
 	worker.StartWorkers(url_ch, rating_ch)
 
-	//testing logic
+	// if can still be read from, rating channel is not closed
+	_, ok := (<-rating_ch)
+	if ok {
+		t.Errorf("rating channel was not closed")
+	}
+}
+
+func TestManagerCreatesRatings(t *testing.T) {
+	url_ch := make(chan string, url_ch_size)
+	rating_ch := make(chan fileio.Rating, rating_ch_size)
+	url_ch <- "https://github.com/lodash/lodash"
+	url_ch <- "https://github.com/nullivex/nodist"
+	url_ch <- "https://github.com/cloudinary/cloudinary_npm"
+
+	worker.StartWorkers(url_ch, rating_ch)
+
+	// comparison to default values of rating type to check
+	// if they were untouched
+	for rating := range rating_ch {
+		if rating.Busfactor == 0 && rating.Correctness == 0 && rating.License == false && rating.NetScore == 0 && rating.Rampup == 0 && rating.Responsiveness == 0 {
+			t.Errorf("ratings were not created correctly")
+		}
+	}
 }
 
 func TestManagerBadInput(t *testing.T) {
 	url_ch := make(chan string, url_ch_size)
 	rating_ch := make(chan fileio.Rating, rating_ch_size)
-	url_ch <- "https://github.com/facebook/react"
 	url_ch <- "https://badurl.com/blabla/test"
-	url_ch <- "https://www.npmjs.com/package/react"
+	url_ch <- "incorrecturl!!@#$**F(S)"
 
 	worker.StartWorkers(url_ch, rating_ch)
 
 	//testing logic
+	for rating := range rating_ch {
+		if rating.Busfactor != 0 && rating.Correctness != 0 && rating.License != false && rating.NetScore != 0 && rating.Rampup != 0 && rating.Responsiveness != 0 {
+			t.Errorf("ratings should not have been created")
+		}
+	}
+}
+
+// inclusion of correct npm url because manager expects converted github url? i think
+func TestManagerNPMUrl(t *testing.T) {
+	url_ch := make(chan string, url_ch_size)
+	rating_ch := make(chan fileio.Rating, rating_ch_size)
+	url_ch <- "https://www.npmjs.com/package/react"
+	url_ch <- "https://www.npmjs.com/package/express"
+
+	worker.StartWorkers(url_ch, rating_ch)
+
+	//testing logic
+	for rating := range rating_ch {
+		if rating.Busfactor != 0 && rating.Correctness != 0 && rating.License != false && rating.NetScore != 0 && rating.Rampup != 0 && rating.Responsiveness != 0 {
+			t.Errorf("ratings should not have been created")
+		}
+	}
 }
--- testfiles/worker_test.go
+++ testfiles/worker_test.go
@@ -11,19 +11,22 @@ import (
 Test to make sure rating channel is filled up
 */
 
-// func TestWorkerRunsTask(t *testing.T) {
-
-// 	//url_ch := make(chan string, rating_ch_size)
-
-// }
-
 func TestWorkerGoodInput(t *testing.T) {
-	rating_ch := make(chan fileio.Rating, rating_ch_size)
+	rating_ch := make(chan fileio.Rating, 1)
 	worker.RunTask("https://github.com/expressjs/express", rating_ch)
-	if len(rating_ch) != 1 {
+	if len(rating_ch) == 0 {
 		t.Errorf("rating channel was not updated")
 	}
+}
 
+func TestWorkerPositiveScore(t *testing.T) {
+	rating_ch := make(chan fileio.Rating, 1)
+	worker.RunTask("https://github.com/nullivex/nodist", rating_ch)
+	for rating := range rating_ch {
+		if rating.NetScore == 0 {
+			t.Errorf("rating should be more than 0")
+		}
+	}
 }
 
 func TestWorkerBadInput(t *testing.T) {
@@ -31,5 +34,33 @@ func TestWorkerBadInput(t *testing.T) {
 	worker.RunTask("https://badurl.com/blabla/test", rating_ch)
 	if len(rating_ch) != 0 {
 		t.Errorf("rating channel should not have been updated")
+	}
+}
+
+func TestWorkerRatingFail(t *testing.T) {
+	rating_ch := make(chan fileio.Rating, 1)
+
+	//change this
+	incorrectUrl := "https://github.com/incorrectownername/react"
+	worker.RunTask(incorrectUrl, rating_ch)
+
+	for rating := range rating_ch {
+		if rating.NetScore != 0 {
+			t.Errorf("rating should have been 0 for: %s", incorrectUrl)
+		}
+	}
+}
+
+func TestWorkerLicenseFail(t *testing.T) {
+	rating_ch := make(chan fileio.Rating, 1)
+
+	//change this
+	incorrectUrl := "https://github.com/expressjs/express"
+	worker.RunTask(incorrectUrl, rating_ch)
+
+	for rating := range rating_ch {
+		if rating.NetScore != 0 && rating.License == false {
+			t.Errorf("rating should have been 0 for: %s", incorrectUrl)
+		}
 	}
 }
